4b11658b499797125bb7abc5db70004d69c5d531
sok thin <phabsokthin992003@gmail.com>
2023-09-21 22:46:49 +0700

    mypofolio

--- src/components/Data.js
+++ src/components/Data.js
@@ -22,6 +22,31 @@ export const CvTitle = [
         title: "Hello Guy",
         title1: "I am a Software Developer",
         title2: "Freelance web designer",
-        title3: "Senior Developer"
+        title3: "Senior Developer",
+        
+        about: "Hello everybody, my name is join smith, I am a software deverloper.",
+        about1: "Nows day, I worked at Google company in USA.",
+        abount2: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+        
     }
+]
+
+export const Profile = [
+
+    {
+        names: "Name:",
+        names1: "Join Smith",
+        email: "Email",
+        email1: "Joinsmith123@gmail.com",
+        phone: "Phone",
+        phone1: "0998388332/0228448332",
+        dob: "Date Of Birth",
+        dob1: "12-03-1992",
+        address: "Address",
+        address1: "ColifonhaSata, 123, USA",
+        button: "HERE ME",
+        button1: "DOWNLOAD CV",
+
+    }
+
 ]
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
 import { CgMenuRight, CgClose } from 'react-icons/cg';
-import { CvTitle, NavData } from './Data';
+import { CvTitle, NavData, Profile } from './Data';
 import NavMobiles from './NavMobiles';
 import { SiGamedeveloper } from 'react-icons/si'
 import { motion } from 'framer-motion'
@@ -64,6 +64,21 @@ const animationRight = {
     }
 }
 
+const scrollAnimatedUp = {
+
+    offscreen: { y: 100, opacity: 0 },
+    onscreen: {
+        y: 0, opacity: 1,
+        transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration: 1
+        }
+
+    }
+
+}
+
 
 function NavigationBar() {
 
@@ -72,22 +87,22 @@ function NavigationBar() {
 
     //for scroll background
 
-    useEffect(()=>{
+    useEffect(() => {
 
-        window.addEventListener('scroll', ()=>{
+        window.addEventListener('scroll', () => {
             return window.scrollY > 50 ? setBg(true) : setBg(false)
         })
-        
+
     })
 
     return (
         <>
 
 
-            <header className={`${bg ? 'bg-blue-700 text-white py-2 lg:py-2' : 'bg-blue'} fixed  left-0 w-full py-8 z-10 transition-all duration-200`}>
+            <header className={`${bg ? 'bg-blue-700 text-white py-2 lg:py-2' : 'bg-blue text-black'} fixed  left-0 w-full py-8 z-10 transition-all duration-200`}>
                 <div className="container mx-auto">
                     <div className='flex justify-between md:justify-between lg:justify-around p-4 '>
-                        <a href="#" className={`${bg ? 'text-white text-4xl' : 'text-orange-500'}  font-modi`}>Porfolio</a>
+                        <a href="#" className={`${bg ? 'text-white text-4xl' : 'text-orange-500 text-4xl'}  font-modi`}>Porfolio</a>
                         <div onClick={() => setMoileNav(!mobileNav)} className='text-2xl md:hidden lg:text-3xl cursor-pointer'>
                             {mobileNav ? <CgClose /> : <CgMenuRight />}
                         </div>
@@ -119,7 +134,7 @@ function NavigationBar() {
             <div className="container mx-auto font-modi">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4">
                     <div className='text-center lg:ml-[14rem] flex justify-center items-center '>
-                         <div className='text-start md:p-5 mt-[8rem] p-4'>
+                        <div className='text-start md:p-5 mt-[8rem] p-4'>
                             {CvTitle.map((item, index) => {
                                 return (
                                     <div key={index}>
@@ -167,7 +182,7 @@ function NavigationBar() {
                                     </div>
                                 )
                             })}
-                        </div> 
+                        </div>
                     </div>
                     <div className='ml-5'>
                         <img src={Cv} className='mt-[8rem]' alt="" style={{ width: "80%" }} />
@@ -175,22 +190,155 @@ function NavigationBar() {
                 </div>
             </div>
 
-            <section className='bg-gray-200/50  mb-3 p-6'>
+            <section className='bg-gray-200/30  mb-3 p-6 font-modi'>
                 <div className="container mx-auto mt-[50px]">
                     <div className='text-center text-2xl mb-3 flex justify-center'>
-                        <p className='border-b-2 border-b-orange-500  ' style={{width:"120px"}}>ABOUT ME</p>
+                        <p className='border-b-2 border-b-orange-500  ' style={{ width: "120px" }}>ABOUT ME</p>
                     </div>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-[5rem]">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-[5rem] ">
                         <div className='flex justify-center'>
-                            <img src={BlueCeo} style={{width: "40%"}} alt="" />
+                            <img src={BlueCeo} className='w-[50%] md:w-[50%] md:object-fit' alt="" />
                         </div>
-                        <div className="bg-red-500">
-                            <h1>Hello world</h1>
+                        <div className="lg:ml-[-4rem]">
+                            {CvTitle.map((item, index) => {
+                                return (
+                                    <div key={index}>
+                                        <motion.p className='lg:text-3xl md:text-2xl'
+
+                                            variants={textAnimationTitle}
+                                            initial={"offscreen"}
+                                            whileInView={"onscreen"}
+                                            viewport={{ once: false, amount: 0.5 }}
+                                            transition={{
+                                                staggerChildren: 0.5
+                                            }}
+
+                                        >{item.about}</motion.p>
+                                        <motion.p className='lg:text-2xl md:text-sm'
+
+                                            variants={animationUp}
+                                            initial={"offscreen"}
+                                            whileInView={"onscreen"}
+                                            viewport={{ once: false, amount: 0.5 }}
+                                            transition={{
+                                                staggerChildren: 0.5
+                                            }}
+
+                                        >{item.about1}</motion.p>
+                                        <motion.div className='mt-2'
+
+
+                                        >
+                                            <motion.p className='text-gray-700/80 md:text-sm'
+
+                                                variants={animationRight}
+
+                                                initial={"offscreen"}
+                                                whileInView={"onscreen"}
+                                                viewport={{ once: false, amount: 0.5 }}
+                                                transition={{
+                                                    staggerChildren: 0.5
+                                                }}
+
+
+                                            >{item.abount2}</motion.p>
+                                        </motion.div>
+                                    </div>
+                                )
+                            })}
+
+                            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-10  ">
+                                <div className="">
+                                    {Profile.map((item, index) => {
+                                        return (
+                                            <div key={index} className='' >
+                                                <motion.div className='space-y-5'
+
+                                                    initial={"offscreen"}
+                                                    whileInView={"onscreen"}
+                                                    viewport={{ once: false, amount: 0.5 }}
+                                                    transition={{ staggerChildren: 0.5 }}
+
+                                                >
+                                                    <motion.p variants={scrollAnimatedUp} className='font-bold'>{item.names}</motion.p>
+                                                    <motion.p variants={scrollAnimatedUp} className='font-bold'>{item.email}</motion.p>
+                                                    <motion.p variants={scrollAnimatedUp} className='font-bold'>{item.dob}</motion.p>
+                                                    <motion.p variants={scrollAnimatedUp} className='font-bold'>{item.address}</motion.p>
+
+
+                                                    <motion.button className='bg-orange-400 rounded-full text-white p-3'
+
+                                                        variants={animationLeft}
+                                                        initial={"offscreen"}
+                                                        whileInView={"onscreen"}
+                                                        viewport={{ once: false, amount: 0.5 }}
+
+
+                                                        whileHover={{
+                                                            scale: 1.1
+                                                        }}
+                                                        transition={{
+                                                            type: "spring",
+                                                            stiffness: 200
+
+                                                        }}
+
+                                                    >{item.button}</motion.button>
+                                                </motion.div>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                                <div className="lg:col-span-3">
+                                    {Profile.map((item, index) => {
+                                        return (
+                                            <div key={index}>
+
+                                                <motion.div className="space-y-5"
+
+                                                    initial={"offscreen"}
+                                                    whileInView={"onscreen"}
+                                                    viewport={{ once: false, amount: 0.5 }}
+                                                    transition={{
+                                                        staggerChildren: 0.5
+                                                    }}
+
+                                                >
+                                                    <motion.p variants={scrollAnimatedUp}>{item.names1}</motion.p>
+                                                    <motion.p variants={scrollAnimatedUp}>{item.email1}</motion.p>
+                                                    <motion.p variants={scrollAnimatedUp}>{item.dob1}</motion.p>
+                                                    <motion.p variants={scrollAnimatedUp}>{item.address1}</motion.p>
+
+                                                    <motion.button className='border-blue-500 border-2 p-3 rounded-full '
+
+                                                        variants={animationRight}
+                                                        initial={"offscreen"}
+                                                        whileInView={"onscreen"}
+                                                        viewport={{ once: false, amount: 0.5 }}
+
+                                                        whileHover={{
+                                                            scale: 1.1
+                                                        }}
+                                                        transition={{
+                                                            type: "keyframes",
+
+                                                        }}
+
+                                                    >{item.button1}</motion.button>
+                                                </motion.div>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            </div>
                         </div>
-                      
+
                     </div>
                 </div>
             </section>
+
+
+
         </>
     )
 }
